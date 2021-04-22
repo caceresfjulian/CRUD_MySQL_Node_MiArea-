442698b65548d239c6f93d8deae856af94ae1b13
Julian Caceres <caceresf.julian@gmail.com>
2021-04-22 02:06:49 -0500

    Add a dummy query

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 // Creación del servidor con Express y Node
 const express = require('express'); 
 
-// const cors = require('cors');
-
 require('dotenv').config({path: './.env'});
 
 const app = express();
 
-require('./database');
-
 app.listen(process.env.PORT, () => {
     console.log('Escuchando por el puerto 4000...')
 });
+
+// Conexión a db
+const client = require('./database');
+
+//Creación de las tablas solicitadas
+client.query('SELECT * from categorias', (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    }
+    client.end();
+})
