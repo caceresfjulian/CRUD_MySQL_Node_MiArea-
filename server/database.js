f5ea9338c4fd363a2f6f158e76f5aa15f2f57e7d
Julian Caceres <caceresf.julian@gmail.com>
2021-04-22 01:13:03 -0500

    Set db connection

---
+++ server/database.js
@@ -0,0 +1,13 @@
+const {Client} = require('pg');
+
+const client = new Client ({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+});
+
+client.connect().then( db => console.log('Tamo en vivo.')).catch(err => console.log(err));
+
+module.exports = client; 
