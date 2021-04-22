d710a66dc2a1b67aff3cdf6cd33d87d675bd7fa3
Julian Caceres <caceresf.julian@gmail.com>
2021-04-21 23:32:33 -0500

    Check repo tracking

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const express = require('express');
 const app = express();
 
 app.listen(4000, () => {
-    console.log('Escuchando por el puerto 4000.')
+    console.log('Escuchando por el puerto 4000...')
 })
