378ac40f9f2afc095d72a13b2d0cc6b44cf13059
Julian Caceres <caceresf.julian@gmail.com>
2021-04-22 01:16:54 -0500

    Require db

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 // const cors = require('cors');
 
+require('dotenv').config({path: './.env'});
+
 const app = express();
 
-app.listen(4000, () => {
+require('./database');
+
+app.listen(process.env.PORT, () => {
     console.log('Escuchando por el puerto 4000...')
-})
+});
