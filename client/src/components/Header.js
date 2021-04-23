e7fc8959c455fd1b8dc193c9d05530419288bc3b
Julian Caceres <caceresf.julian@gmail.com>
2021-04-23 15:01:10 -0500

    Create Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <h3>CRUD MiArea</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
