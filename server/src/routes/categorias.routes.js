faf98c4b70bbf2bfdcfc2b6f0e8efb626c613583
Julian Caceres <caceresf.julian@gmail.com>
2021-04-22 20:39:49 -0500

    Set routes using a Pool

--- server/src/routes/categorias.routes.js
+++ server/src/routes/categorias.routes.js
@@ -1,40 +1,20 @@
 const router = require('express').Router();
-const client = require('../database');
-let categorias = [];
-
-// Obtener la lista de categorías.
-router.get('/', async (req, res) => {
-    try {
-        await client.connect();
-        client.query('SELECT * FROM categorias', (err, result) => {
-            console.log(result.rows);
-            client.end();
-            categorias = result.rows;
-            res.json(categorias);
-        });
-    }
-    catch (error) {
-        console.log(error);
-    }
-});
+const pool = require('../database');
 
 // Crear una categoría nueva.
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     try {
         const { nombre_categoria } = req.body;
 
         // Verificar si existe otro registro con mismo nombre.
         const query = `SELECT * FROM categorias WHERE nombre_categoria='${nombre_categoria}';`;
-        await client.connect();
-        client.query(query, (err, result) => {
+        pool.query(query, (err, result) => {
             if (result.rows.length > 0) {
-                client.end();
                 return res.status(401).send();
             } else {
                 // Despues de verificado, se crea el registro.
-                client.query(`INSERT INTO categorias (nombre_categoria) VALUES ('${nombre_categoria}')`,
+                pool.query(`INSERT INTO categorias (nombre_categoria) VALUES ('${nombre_categoria}')`,
                     (err, result) => {
-                        client.end();
                         return res
                             .json({ mensaje: `Categoría ${nombre_categoria} creada.` })
                             .status(201)
@@ -53,17 +33,11 @@ router.delete('/:id', async (req, res) => {
     const id = req.params.id;
     const query = `DELETE FROM categorias WHERE id = ${id}`;
     try {
-        await client.connect();
-        client.query(query, (err, result) => {
-            client.end();
-            res
-                .json({ mensaje: 'Registro eliminado.' })
-                .status(204)
-                .send();
+        pool.query(query, (err, result) => {
+            res.json({ mensaje: 'Registro eliminado.' });
         })
     }
     catch (error) {
-        client.end();
         console.log(error);
     }
 });
@@ -75,8 +49,7 @@ router.put('/', async (req, res) => {
     const query = `UPDATE categorias SET nombre_categoria ='${nuevo_nombre}' WHERE id=${id}`;
 
     try {
-        await client.connect();
-        client.query(query, (err, result) => {
+        pool.query(query, (err, result) => {
             if (!err) {
                 res
                     .json({ mensaje: `Categoría actualizada a ${nuevo_nombre}` })
