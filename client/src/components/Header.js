54482382aae4d40e4c2e12bb594c5b216cdab9d1
Julian Caceres <caceresf.julian@gmail.com>
2021-04-23 15:22:30 -0500

    Modify Navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <h3>CRUD MiArea</h3>
+                <Link className="nav-link" to="/categorias">Categorías</Link>
+                <Link className="nav-link" to="/articulos">Artículos</Link>
             </div>
         </nav>
     )
