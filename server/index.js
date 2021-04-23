5309ffb3c9b4e17ae1d0aeac1a71e8ff4de8a806
Julian Caceres <caceresf.julian@gmail.com>
2021-04-22 23:13:19 -0500

    Set CORS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 // Creación del servidor con Express y Node
-const express = require('express'); 
+const express = require('express');
 
-require('dotenv').config({path: './.env'});
+require('dotenv').config({ path: './.env' });
 
 const app = express();
 
+const cors = require('cors');
+
 app.use(express.json());
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
 
 // Conexión a db
 // require('./src/database');
